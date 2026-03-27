24b9b8890123e2563a06a9a2d3d12d99011e1a35
Amber Zhang <zhangsiyue343@gmail.com>
2026-03-27 19:38:26 +0000

    Update day30_MiniDexPair.sol

--- Amber/day30_MiniDexPair.sol
+++ Amber/day30_MiniDexPair.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/utilS/ReentrancyGuard.sol";
 
 /// @title MiniDexPair - 简单去中心化交易对（DEX）合约
 /// @notice 支持添加/移除流动性、代币交换、LP份额管理
